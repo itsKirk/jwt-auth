6e3b28f6f6467edc187d55425b402197ff8c9468
Kirk <kirkrepository@gmail.com>
2023-06-29 00:05:50 +0300

    db file

--- server/data/db.js
+++ server/data/db.js
@@ -1,25 +1,21 @@
 const mongoose = require("mongoose");
 const Country = require("../models/country");
 const { countryData } = require("./countries");
-const MongoDB_URL=process.env
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost/jwt-auth", {
+    await mongoose.connect(process.env.MongoDB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
     console.log("Connected to MongoDB");
 
-    // Check if the "countries" collection is empty
     const count = await Country.countDocuments();
     if (count === 0) {
-      // Seed the collection with countries from the file
       await Country.insertMany(countryData);
       console.log("Countries collection seeded");
     }
   } catch (error) {
-    console.error("Failed to connect to MongoDB", error);
     process.exit(1);
   }
 };
